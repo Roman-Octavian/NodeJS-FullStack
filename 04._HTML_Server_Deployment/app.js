eb7e8d3d3ae4d4f0e3f90a4fbfc1efecc96e3111
Roman-Octavian <work.octavian@gmail.com>
2023-03-02 10:40:17 +0100

    Added week 4 homework

---
+++ 04._HTML_Server_Deployment/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+const PORT = 8080;
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log("Server is running on port", PORT);
+});
